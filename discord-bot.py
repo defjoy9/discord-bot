19b628d66714cd764fd0fc0748da9c38e92411b0
defjoy <94872697+defjoy9@users.noreply.github.com>
2024-11-30 21:01:44 +0100

    Now bot can respond to a custom message

--- discord-bot.py
+++ discord-bot.py
@@ -1,4 +1,7 @@
+# bot.py
 import os
+import random
+
 import discord
 from dotenv import load_dotenv
 
@@ -6,18 +9,38 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 #GUILD = os.getenv('DISCORD_GUILD')
 
-intents = discord.Intents.default()  # Use default intents
-intents.members = True 
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
+#greets new guild member
 @client.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my server!'
+        f'Hi {member.name}, welcome to my Discord server!'
     )
+
+# responds to a message
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    choices = [
+        'YUH!',
+        (
+            'Ariana Grande has the best yuh'
+        ),
+    ]
+
+    if message.content == 'yuh':
+        response = random.choice(choices)
+        await message.channel.send(response)
+
 client.run(TOKEN)
