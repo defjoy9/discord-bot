2f396a3b756486a78c29a8416f1a28ac740270ac
defjoy <94872697+defjoy9@users.noreply.github.com>
2024-11-30 21:30:11 +0100

    Changed @client to @bot for better usage and upgarded response to a message command

--- discord-bot.py
+++ discord-bot.py
@@ -1,46 +1,37 @@
 # bot.py
 import os
 import random
-
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-#GUILD = os.getenv('DISCORD_GUILD')
+GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!',intents=intents)
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
-
-#greets new guild member
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
+    print(f'{bot.user.name} has connected to {GUILD}!')
 
 # responds to a message
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
+@bot.command(name='yuh', help='Responds with a random text about Ariana Grande')
+async def yuh(ctx):
     choices = [
         'YUH!',
-        (
-            'Ariana Grande has the best yuh'
-        ),
+        "did you know that, she was born in 1993?",
+        "did you know that, she was born in Florida?",
+        "did you know that, she's from US?",
+        "did you know that, she's a singer?",
+        "did you know that, she's an inventor of 'yuh'?"
     ]
+    response = random.choice(choices)
+    await ctx.send(response)
 
-    if message.content == 'yuh':
-        response = random.choice(choices)
-        await message.channel.send(response)
 
-client.run(TOKEN)
+bot.run(TOKEN)
