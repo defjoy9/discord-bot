ead129108d82391a988538d20fba90b61c67d35e
defjoy <94872697+defjoy9@users.noreply.github.com>
2024-11-30 20:39:31 +0100

    simplified utility and now it has also guild name

--- discord-bot.py
+++ discord-bot.py
@@ -4,12 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.default()  # Use default intents
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 client.run(TOKEN)
